356cc7b4587e4d7242644cf3157219ae06acb969
Miguel <miguelaugustosena@gmail.com>
2024-12-13 11:06:38 -0300

    final

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-timer',
@@ -8,13 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './timer.component.css',
 })
 export class TimerComponent {
-  nomeEvento: string = "New Year";
+  nomeEvento: string = 'New Year';
   month: string = '0';
   day: string = '0';
   hour: string = '0'.padStart(2, '0');
   minute: string = '0'.padStart(2, '0');
   second: string = '0'.padStart(2, '0');
   private intervalId!: ReturnType<typeof setInterval>;
+  event: Date = new Date('2025-01-01T00:00:00');
+  now: Date = new Date();
+  nextDay: Date = new Date();
 
   ngOnInit(): void {
     this.intervalId = setInterval(() => {
@@ -26,23 +29,37 @@ export class TimerComponent {
     clearInterval(this.intervalId);
   }
 
+  addOneDay(): Date {
+    this.nextDay.setDate(this.nextDay.getDate() + 1);
+    return this.nextDay;
+  }
+
   private updateTime(): void {
-    const now = new Date();
-    const event = new Date('2025-01-01T00:00:00');
-    var diff = Math.abs(event.getTime() - now.getTime());
-    this.month = this.padWithZero(Math.floor(Number(diff) / (360000000 * 30)));
-    diff -= Number(this.month) * 360000000 * 30;
-    this.day = this.padWithZero(Math.floor(Number(diff) / (3600000 * 24)));
-    diff -= Number(this.day) * 3600000 * 24;
-    this.hour = this.padWithZero(Math.floor(Number(diff) / 3600000));
-    diff -= Number(this.hour) * 3600000;
-    this.minute = this.padWithZero(Math.floor(Number(diff) / 60000));
-    diff -= Number(this.minute) * 60000;
-    this.second = this.padWithZero(Math.floor(Number(diff) / 1000));
-    diff -= Number(this.second) * 1000;
+    this.now = new Date();
+    const diff = Math.abs(this.event.getTime() - this.now.getTime());
+    const totalDays = Math.floor(diff / (3600000 * 24));
+    this.month = this.padWithZero(Math.floor(totalDays / 30));
+    const remainingDays = totalDays % 30;
+    this.day = this.padWithZero(remainingDays);
+    this.hour = this.padWithZero(Math.floor((diff % (3600000 * 24)) / 3600000));
+    this.minute = this.padWithZero(Math.floor((diff % 3600000) / 60000));
+    this.second = this.padWithZero(Math.floor((diff % 60000) / 1000));
   }
 
   private padWithZero(value: number): string {
     return value.toString().padStart(2, '0');
+  }
+
+  onClick(data: string): void {
+    this.nomeEvento = data;
+    if (this.nomeEvento == 'New Year') {
+      this.event = new Date('2025-01-01T00:00:00');
+    } else if (this.nomeEvento == 'Xmas') {
+      this.event = new Date('2024-12-25T00:00:00');
+    } else {
+      this.event = new Date(data + 'T00:00:00');
+      this.nomeEvento = 'Custom ✏️';
+    }
+    this.updateTime();
   }
 }
